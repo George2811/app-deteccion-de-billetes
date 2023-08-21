9d72def7215136f117da499e5cc42fbda0e0b4ec
George2811 <sanchez28112001@gmail.com>
2023-08-20 19:19:58 -0500

    terminada guide view y splash view

--- lib/ui/views/characteristics.dart
+++ lib/ui/views/characteristics.dart
@@ -57,17 +57,18 @@ class _CharacteristicsViewState extends State<CharacteristicsView> {
             ),
           ),
       body: CarouselSlider(
-        options: CarouselOptions(height: 540.0, viewportFraction: 0.9),
+        options: CarouselOptions(height: 590.0, viewportFraction: 0.9, enlargeCenterPage: true, enlargeFactor: 0.15),
         items: characteristics.map((e) {
           return Builder(
             builder: (BuildContext context) {
               return Container(
                 width: MediaQuery.of(context).size.width,
-                margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                margin: const EdgeInsets.symmetric(horizontal: 2.0),
                 child: Card(
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10.0),
                   ),
+                  color: const Color.fromARGB(255, 9, 20, 16),
                   child: Column(
                     children: [
                       ClipRRect(
@@ -83,7 +84,7 @@ class _CharacteristicsViewState extends State<CharacteristicsView> {
                           )
                         ),
                       ),
-                      const SizedBox(height: 20,),
+                      const SizedBox(height: 20,),                     
                       Container(
                         margin: const EdgeInsets.symmetric(horizontal: 20),
                         alignment: Alignment.centerLeft,
@@ -91,8 +92,41 @@ class _CharacteristicsViewState extends State<CharacteristicsView> {
                           e['name']?? "Titulo",
                           style: const TextStyle(
                             fontWeight: FontWeight.bold,
-                            fontSize: 16,
+                            color: Colors.white,
+                            fontSize: 18,
                           ),
+                        ),
+                      ),
+                      const SizedBox(height: 20,),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Row(                        
+                          children: [
+                            Container(
+                              alignment: Alignment.center,                   
+                              padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 10),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                color: Colors.white,
+                              ),
+                              child: Text(
+                                "${e['amount']?? 'Monto en'} ${widget.category.toLowerCase()}",
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 20,),
+                            Text(
+                              "Edición: ${e['edition']}",
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white,
+                                fontSize: 14,
+                              ),
+                            )
+                          ],
                         ),
                       ),
                       const SizedBox(height: 20,),
@@ -102,7 +136,7 @@ class _CharacteristicsViewState extends State<CharacteristicsView> {
                           e['description']?? "Hilo de seguridad con movimiento de anillos, que cambian de color fucsia a verde y el texto 100 BCRP calado. El hilo contiene elementos magnéticos por lo que es detectable por máquina.",
                           textAlign: TextAlign.justify,
                           style: const TextStyle(
-                            color: Colors.grey,
+                            color: Color.fromARGB(255, 202, 202, 202),
                           ),
                         ),
                       )
---
+++ lib/ui/views/splash.dart
@@ -0,0 +1,108 @@
+import 'package:counterfeit_detector/ui/views/login.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // Simula una espera de 3 segundos antes de navegar a la siguiente pantalla.
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginView()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 450,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              image: DecorationImage(
+                image: const AssetImage("assets/cover.jpg"),
+                fit: BoxFit.cover,
+                
+                colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.7),
+                  BlendMode.darken,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  stops: [0.7, 1],
+                  colors: [Color.fromARGB(255, 1, 9, 3), Color.fromARGB(255, 2, 38, 12)],
+                )
+              ),
+              child: Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.all(15),
+                child: RichText(
+                  text: const TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Detector de\n',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 35,
+                        ),
+                      ),
+                       TextSpan(
+                        text: 'billetes falsos\n\n',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 35,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'Detecta la veracidad de tus billetes,\n',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 190, 190, 190),
+                          fontSize: 15,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'tanto soles peruanos como\n',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 190, 190, 190),
+                          fontSize: 15,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'dólares americanos.',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 190, 190, 190),
+                          fontSize: 15,
+                        ),
+                      ),
+                    ]
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
